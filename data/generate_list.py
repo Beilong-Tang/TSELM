12a2ebba07c7daf005435d45b2ceb835c16aae45
Beilong Tang <bt132@duke.edu>
2024-09-14 14:15:25 +0800

    finish data file generating

--- data/generate_list.py
+++ data/generate_list.py
@@ -6,8 +6,10 @@ import tqdm
 
 BASE_PATH = op.abspath(op.dirname(__file__))
 
+
 def p(*args):
     return op.join(BASE_PATH, *args)
+
 
 os.makedirs(p("list"), exist_ok=True)
 os.makedirs(p("list", "train"), exist_ok=True)
@@ -15,8 +17,9 @@ os.makedirs(p("list", "dev"), exist_ok=True)
 os.makedirs(p("list", "test"), exist_ok=True)
 
 train_audio = [p("librispeech", i) for i in ["train-clean-100", "train-clean-360"]]
-dev_audio = "dev-clean"
-test_audio = "test-clean"
+dev_audio = "libri2mix_dev"
+test_audio = "libri2mix_test"
+
 
 def generate_training_pt():
     print("generate training scp")
@@ -32,13 +35,20 @@ def generate_training_pt():
     torch.save(spk_dict, p("list", "train", "train_100_360.pt"))
     print("done!")
 
-def generate_scp(name:str):
-    print(f"generating scp for {name}")
-    
 
-    print("done!")
-    pass
+def generate_scp(dataset_name: str, type: str):
+    print(f"generating scp for {dataset_name} of type {type}")
+    files = sorted(glob.glob(p(dataset_name, type, "*.wav")))
+    res = [f"{i.split('/')[-1]} {i}\n" for i in files]
+    with open(p("list", dataset_name, f"{type}.scp"), "w") as f:
+        for r in res:
+            f.write(r)
+    print("done")
 
 
 # generate_training_pt()
-generate_scp()
+
+for t in ["aux_s1", "mix_clean", "s1"]:
+    for d in [dev_audio, test_audio]:
+        generate_scp(d, t)
+print("All scp files are generated successfully!")
