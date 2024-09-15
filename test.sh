ef5399651510609142ec37601b50cd0ab3137f03
Beilong Tang <bt132@duke.edu>
2024-09-15 10:12:48 +0800

    remove testing code

--- test.sh
+++ test.sh
@@ -1,27 +1,26 @@
 python inference.py -scp /public/home/qinxy/bltang/data/LibriMix/Libri2Mix/wav16k/min/lists/dev -config ./config/tselm_l.yaml -ckpt /public/home/qinxy/bltang/ml_framework_slurm/exp/tsslm_final/discrete_k_1000/ckpt/conf_l/best.pth -d cuda:0 -o /public/home/qinxy/bltang/TSELM/output/tselm_l -gpus cuda:0 cuda:1 cuda:2 cuda:3 -proc 8 
 
-
-rank 3 get spk1 1413
+rank 1 get spk1 5163
 rank 0 get spk1 1316
+rank 2 get spk1 5092
 rank 1 get spk1 770
-rank 2 get spk1 8183
-rank 2 get spk1 511
+rank 3 get spk1 1263
 rank 0 get spk1 7240
-rank 3 get spk1 3289
-rank 1 get spk1 8498
-rank 3 get spk1 1961
+rank 2 get spk1 3866
+rank 3 get spk1 6272
 rank 1 get spk1 7938
-rank 2 get spk1 8534
+rank 3 get spk1 731
 rank 0 get spk1 7188
-rank 1 get spk1 6574
+rank 1 get spk1 362
+rank 2 get spk1 492
+rank 2 get spk1 6098
 rank 1 get spk1 2512
-rank 3 get spk1 5022
-rank 0 get spk1 7011
-rank 2 get spk1 984
-rank 1 get spk1 196
-rank 2 get spk1 5514
-rank 3 get spk1 597
-rank 0 get spk1 543
 rank 0 get spk1 1624
-rank 3 get spk1 6538
-rank 2 get spk1 1098
+rank 2 get spk1 5126
+rank 0 get spk1 7011
+rank 3 get spk1 2061
+rank 2 get spk1 6509
+rank 3 get spk1 3187
+rank 1 get spk1 1624
+rank 3 get spk1 2960
+rank 0 get spk1 543
