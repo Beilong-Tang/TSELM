cb97de44871e7ca1d3957759b5e17cbb5a79ce38
Beilong Tang <bt132@duke.edu>
2024-09-15 10:09:32 +0800

    minor fix

--- utils/io.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def make_path(file_path, is_dir=True):
-    if is_dir:
-        os.makedirs(file_path, exist_ok=True)
-    else:
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    return file_path
